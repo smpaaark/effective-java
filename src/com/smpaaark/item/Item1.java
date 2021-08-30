470a4ba795177aef8d0e4b9adcc53782a791c1f5
smith <crm90024@lotte.net>
2021-08-30 16:48:52 +0900

    Item1

---
+++ src/com/smpaaark/item/Item1.java
@@ -0,0 +1,18 @@
+package com.smpaaark.item;
+
+import java.util.EnumSet;
+
+public class Item1 {
+
+    public static void main(String[] args) {
+        Boolean booleanVal = Boolean.valueOf(false);
+        if (booleanVal) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        EnumSet<SampleEnum> sampleEnums = EnumSet.noneOf(SampleEnum.class);
+    }
+
+}
---
+++ src/com/smpaaark/item/SampleEnum.java
@@ -0,0 +1,7 @@
+package com.smpaaark.item;
+
+public enum SampleEnum {
+
+    data1, data2, data3;
+
+}

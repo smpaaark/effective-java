d3952970abee989deb4d59f7849edee6025df80d
smpark1020 <smpark911020@gmail.com>
2021-09-06 12:48:07 +0900

    [Effective Java] 아이템8 - finalizer와 cleaner 사용을 피하라

---
+++ src/com/smpaaark/item8/Adult.java
@@ -0,0 +1,12 @@
+package com.smpaaark.item8;
+
+// cleaner 안전망을 갖춘 자원을 제대로 활용하는 클라이언트 (45쪽)
+public class Adult {
+
+    public static void main(String[] args) throws Exception {
+        try (Room myRoom = new Room(7)) {
+            System.out.println("안녕~");
+        }
+    }
+
+}
